17519384ca941f2197e0007c102e4bb8ffb5ea1b
Hedi Belkahia <hedibelkahia03@gmail.com>
2024-06-16 00:39:36 +0100

    Add new button components

---
+++ src/components/Buttons/SuccessButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <div class="buttons">
+        <button class="
+          mx-2 
+          rounded px-4 py-2 
+          transition duration-300 
+          ease-in-out 
+          text-white border 
+          bg-green-700 
+          border-green-700 
+          hover:bg-white 
+          hover:text-green-700">
+          <font-awesome-icon :icon="['fas', 'check']" /> 
+          Success
+        </button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SucessButton'
+  }
+  </script>
+  
---
+++ src/components/Buttons/WarningButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <div class="buttons">
+        <button class="
+        mx-2 
+        rounded px-4 py-2 
+        transition duration-300 ease-in-out 
+        text-white border 
+        bg-yellow-500 
+        border-yellow-500 
+        hover:bg-white 
+        hover:text-yellow-500">
+        <font-awesome-icon :icon="['fas', 'exclamation-triangle']" /> 
+        Warning
+      </button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'WarningButton'
+  }
+  </script>
+  
