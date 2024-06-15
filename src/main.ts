260dbf5d01f8779a475e3a82b01a3b91a137d80e
Hedi Belkahia <hedibelkahia03@gmail.com>
2024-06-16 00:38:37 +0100

    Add vue-router pages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+import { createWebHistory, createRouter } from 'vue-router'
 import App from "./App.vue";
 import "./index.css";
-
+import Home from "./pages/Home.vue";
+import Buttons from "./pages/Buttons.vue";
+import Graphs from "./pages/Graphs.vue";
 
 ///// fontawesome /////
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -13,6 +16,20 @@ library.add(faCheck, faTriangleExclamation, faXmark)
 
 
 
+////// router //////
+const routes = [
+    { path: '/', component: Home },
+    { path: '/Buttons', component: Buttons },
+    { path: '/Graphs', component: Graphs }
+]
+  
+const router = createRouter({
+    history: createWebHistory(),
+    routes : routes
+})
+////////
+
 createApp(App)
+.use(router)
 .component('font-awesome-icon', FontAwesomeIcon)
 .mount("#app");
---
+++ src/pages/Graphs.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Soon...</h1>
+    </div>
+</template>
