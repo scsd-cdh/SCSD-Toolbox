4e0c814e7832f2fc2c2b7d20a98720461f201c40
Hedi Belkahia <hedibelkahia03@gmail.com>
2024-07-20 13:12:26 +0200

    Add Lucide Vue Next icons

--- src/pages/Buttons.vue
+++ src/pages/Buttons.vue
@@ -5,15 +5,26 @@
         <Button variant="link">Link</Button>
         <Button variant="destructive">Destructive</Button>
         <Button variant="ghost">ghost</Button>
+        <Button variant="secondary">
+            <House class="w-4 h-4"/>
+        </Button>
+        <Button>
+            <Mail class="w-4 h-4 mr-2"/>Icon
+        </Button>
+
     </section>
 </template>
 
 <script>
-    import { Button } from '@/components/button'
+    import { Button } from "@/components/button"
+    import { House } from 'lucide-vue-next'
+    import { Mail } from 'lucide-vue-next'
 
     export default {
         components: {
             Button,
+            House,
+            Mail,
         }
     }
 </script>
