a29dcc3ec82ef3b154cc81fe9f5eb4e0f72f72a6
Hedi Belkahia <hedibelkahia03@gmail.com>
2024-07-21 13:59:03 +0200

    Update Table example page

--- src/pages/Tables.vue
+++ src/pages/Tables.vue
@@ -7,28 +7,37 @@
                     <TableHead>First Name</TableHead>
                     <TableHead>Last Name</TableHead>
                     <TableHead>Email</TableHead>
+                    <TableHead>Status</TableHead>
                 </TableRow>
             </TableHeader>
-            <TableBody>
+            <TableBody class="text-left">
                 <TableRow>
                     <TableCell>John</TableCell>
                     <TableCell>Doe</TableCell>
                     <TableCell>abc@gmail.com</TableCell>
+                    <TableCell>
+                        <Badge>Active</Badge>
+                    </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell>Jane</TableCell>
                     <TableCell>Doe</TableCell>
                     <TableCell>fcg@gmail.com</TableCell>
+                    <TableCell>
+                        <Badge variant="destructive">Inactive</Badge>
+                    </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell>John</TableCell>
                     <TableCell>Smith</TableCell>
                     <TableCell>pol@gmail.com</TableCell>
+                    <TableCell>
+                        <Badge>Active</Badge>
+                    </TableCell>
                 </TableRow>
             </TableBody>
         </Table>
     </section>
-    
 </template>
 
 <script>
@@ -41,6 +50,7 @@
     TableRow,
     TableCaption,
   } from '@/components/table'
+    import { Badge } from '@/components/badge'
 
     export default {
         components: {
@@ -51,6 +61,7 @@
             TableHeader,
             TableRow,
             TableCaption,
+            Badge,
         }
     }
 </script>
